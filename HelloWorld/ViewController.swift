42e8e19139e242512d1ec5895fc78614289634fb
IliaKolosov <ilyxa-extrimal@yandex.ru>
2023-04-23 12:28:53 +0300

    trafficLigth

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var orangeLight: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonStart() {
+    }
+    
 }
 
