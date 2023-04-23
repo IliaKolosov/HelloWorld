3a053be97f09f8adf270ff85da9ed22ac3eac471
IliaKolosov <ilyxa-extrimal@yandex.ru>
2023-04-23 23:41:11 +0300

    fanal commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,19 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, orange, green
+}
 
+final class ViewController: UIViewController {
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var greenLight: UIView!
     @IBOutlet var orangeLight: UIView!
     
+    private var currentLight = CurrentLight.red
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
+    @IBOutlet var buttonStart: UIButton!
+    private var cornerRadius: CGFloat = 90
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
-
-    @IBAction func buttonStart() {
+        redLight.layer.cornerRadius = cornerRadius
+        greenLight.layer.cornerRadius = cornerRadius
+        orangeLight.layer.cornerRadius = cornerRadius
+        
+        redLight.alpha = lightOff
+        orangeLight.alpha = lightOff
+        greenLight.alpha = lightOff
     }
     
+    @IBAction func buttonStartTappet() {
+        if buttonStart.currentTitle == "Start"{
+            buttonStart.setTitle("Next", for: .normal)
+            
+        }
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightOff
+            redLight.alpha = lightOn
+            currentLight = .orange
+        case .orange:
+            redLight.alpha = lightOff
+            orangeLight.alpha = lightOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightOn
+            orangeLight.alpha = lightOff
+            currentLight = .red
+        }
+    }
 }
 
